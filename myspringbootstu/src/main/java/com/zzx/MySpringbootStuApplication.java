ab56837e22206d077aecdc1935232319e2da9f1f
zhongxian111 <zhzhxian@163.com>
2021-04-22 23:09:06 +0800

    spring boot从零开始吃透

---
+++ myspringbootstu/src/main/java/com/zzx/MySpringbootStuApplication.java
@@ -0,0 +1,11 @@
+package com.zzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootStuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootStuApplication.class);
+    }
+}
